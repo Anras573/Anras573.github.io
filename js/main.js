ea8557794f8f02283a5ab3daa04f1a03c958adbf
Anders Bo Rasmussen <andersborasmussen@hotmail.com>
2015-04-22 19:25:06 +0200

    Spelling, CSS and JavaScript
    Fixed Spelling error, removed unused footer code, remove custom styling
    and added a CSS class instead, rewrote some javascript and made scroll
    animation slower.

--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
 
-function getHash () {
-	if (location.hash) {
-		var hash = location.hash.replace('#', '');
-		fader(hash);
-	} else {
-		var hash = 'experience';
-		fader(hash);
-	}
+function getHash() {
+    if (location.hash) {
+        var hash = location.hash.replace('#', '');
+        fade(hash);
+    } else {
+        var hash = 'experience';
+        fade(hash);
+    }
 }
-function fader (target) {
-	$('#menu li').removeClass('active');
-	$('#menu a[href=#' + target +']').parent('li').addClass('active');
-	$('.content').fadeOut(200, function () {
-		$('.content > div').addClass('not-active').removeClass('active');
-		$('.content .' + target).addClass('active').removeClass('not-active');
-		$('.content').fadeIn(200, function () {
-			if ($(document).width() <= 770 && location.hash){
-				$(window).scrollTop($('#menu').height()+$('.header-container').innerHeight());
-			}
-		});
-	});
+function fade(target) {
+    $('#menu li').removeClass('active');
+    $('#menu a[href=#' + target + ']').parent('li').addClass('active');
+    $('.content').fadeOut(200, function () {
+        $('.content > div').addClass('not-active').removeClass('active');
+        $('.content .' + target).addClass('active').removeClass('not-active');
+        $('.content').fadeIn(200, function () {
+            if ($(document).width() <= 770 && location.hash) {
+                $('html, body').animate({
+                    scrollTop: $('#menu').height() + $('.header-container').innerHeight()
+                }, 'slow');
+            }
+        });
+    });
 }
-$(document).ready(function() {
-	getHash();
-	window.onhashchange = function() {
-		getHash();
-	};
-	$('#menu li').on('click', function() {
-		var href = $(this).find('a').attr('href');
-		if (window.location.origin.indexOf('undefined') !== -1) {
-			window.location.href = window.location.origin.replace('undefined','') + '/' + href;
-		} else {
-			window.location.href = window.location.origin + '/' + href;
-		}
-	});
-	$('#menu li a').on('click', function(event) {
-		event.stopPropagation();
-	});
+$(document).ready(function () {
+    getHash();
+    window.onhashchange = function () {
+        getHash();
+    };
+    $('#menu li').on('click', function () {
+        var href = $(this).find('a').attr('href');
+        if (window.location.origin.indexOf('undefined') !== -1) {
+            window.location.href = window.location.origin.replace('undefined', '') + '/' + href;
+        } else {
+            window.location.href = window.location.origin + '/' + href;
+        }
+    });
+    $('#menu li a').on('click', function (event) {
+        event.stopPropagation();
+    });
 });
