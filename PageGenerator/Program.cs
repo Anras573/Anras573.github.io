8ac555e2a2914bf5d17fb4035e8ee1cce040cdfc
Anders Bo Rasmussen <andersborasmussen@hotmail.com>
2022-04-16 20:46:54 +0200

    Initial commit.

---
+++ PageGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Statiq.App;
+using Statiq.Web;
+
+Console.WriteLine("Hello, World!");
+
+await Bootstrapper
+    .Factory
+    .CreateWeb(new[] { "preview" })
+    .RunAsync();
---
+++ PageGenerator/input/assets/js/app.js
@@ -0,0 +1,10 @@
+﻿const themeToggler = document.querySelector(".theme-toggler");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+themeToggler.addEventListener("click", function () {
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+    } else {
+        document.body.classList.toggle("dark-theme");
+    }
+});
