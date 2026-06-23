e7772263d4ff2ad00626b85a97c3892d821ac950
Anders Bo Rasmussen <anras573@gmail.com>
2026-06-23 13:10:12 +0200

    feat: add projects section to resume and include in index
    Signed-off-by: Anders Bo Rasmussen <anras573@gmail.com>

--- _data/resume.js
+++ _data/resume.js
@@ -28,7 +28,8 @@ export default async function() {
             work: [],
             volunteer: [],
             education: [],
-            skills: []
+            skills: [],
+            projects: []
         };
     }
 }
